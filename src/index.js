c97dac34da51709a13dad0676d49cd06f03b5d0a
Divyansh Kumar <divyanshkrit@gmail.com>
2022-01-08 03:06:31 +0530

    pkg

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
